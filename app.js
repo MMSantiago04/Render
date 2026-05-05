f8ce75e7453b51d807361d0097a01c3252278aa6
MMSantiago04 <universidadproyectos56@gmail.com>
2026-05-04 23:00:56 -0500

    Update3 app.js

--- app.js
+++ app.js
@@ -1,12 +1,28 @@
 const express = require("express");
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("FUNCIONA RAIZ 🚀");
+app.use(express.json());
+
+// 🔹 VERIFICACIÓN
+app.get("/webhook", (req, res) => {
+  const VERIFY_TOKEN = "mi_token_123";
+
+  const mode = req.query["hub.mode"];
+  const token = req.query["hub.verify_token"];
+  const challenge = req.query["hub.challenge"];
+
+  if (mode === "subscribe" && token === VERIFY_TOKEN) {
+    console.log("WEBHOOK VERIFIED ✅");
+    res.status(200).send(challenge);
+  } else {
+    res.sendStatus(403);
+  }
 });
 
-app.get("/webhook", (req, res) => {
-  res.send("FUNCIONA WEBHOOK 🔥");
+// 🔹 RECIBIR MENSAJES
+app.post("/webhook", (req, res) => {
+  console.log("Mensaje recibido:", JSON.stringify(req.body, null, 2));
+  res.sendStatus(200);
 });
 
 app.listen(process.env.PORT || 3000);
